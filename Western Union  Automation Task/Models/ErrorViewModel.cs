064970cc0ef5e87466f337ada67bf5670be6f26c
Rohul Amin Mohammad Dipu <rad.edu.du@gmail.com>
2026-05-26 18:03:20 +0600

    created the project

---
+++ Western Union  Automation Task/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Western_Union__Automation_Task.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
